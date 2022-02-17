d87a7d2b95da506772c17b9c8b92cc4de531ab01
liu-yucheng <liu.r.yucheng@gmail.com>
2022-02-18 03:23:32 +0800

    Updated create-dev and create-rel

--- lyc-git-bash-utils/exes/gbu-create-rel.bash
+++ lyc-git-bash-utils/exes/gbu-create-rel.bash
@@ -3,6 +3,8 @@
 # "gbu create-rel" command executable.
 #
 # Can be executed directly.
+# Finds the main branch name.
+# Goes to the main branch.
 # Tries to create a rel (release) branch.
 # Goes to the rel branch.
 
@@ -22,6 +24,11 @@ if [[ -z $_gbu_incl_exes ]]; then
     eval "$_gbu_ensure_metainfo_eval"
 fi
 
+if [[ -z $_gbu_incl_libs_defaults ]]; then
+    source $__dir/../libs/defaults.bash
+    eval "$_gbu_ensure_metainfo_eval"
+fi
+
 __main() {
     local brief_usage="gbu create-rel"
     local few_args_fmt="\"$brief_usage\" gets too few arguments\nExpects 0 arguments; Gets %s arguments\n"
@@ -33,9 +40,34 @@ __main() {
         echo -e "$usage" >|/dev/stderr
         exit 1
     elif [[ $# -eq 0 ]]; then
+        touch $_gbu_main_names_loc
+        local main_names=$(cat $_gbu_main_names_loc)
+        local main_name_list=()
+        local ifs_backup=$IFS
+        IFS=$'\n'
+
+        while [[ 1 ]]; do
+            local line
+            read line
+
+            if [[ $? -ne 0 ]]; then
+                break
+            fi
+
+            main_name_list+=("$line")
+        done <<<$main_names
+
+        IFS=$ifs_backup
+        local main_name=${main_name_list[0]}
+
+        if [[ -z "$main_name" ]]; then
+            local main_name=main
+        fi
+
         (
             set -o xtrace # Turn command tracing on
 
+            git checkout $main_name
             git branch rel
             git checkout rel
             git branch
